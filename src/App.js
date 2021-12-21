8f532f56810ad1f41401fb67153e7ecf424b110e
ewsgit <ews2909@outlook.com>
2021-12-21 19:12:08 +0000

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import React from "react";
+import styled from "styled-components";
+
+const Page = styled.div`
+background-color: #454545;
+  > .nav {
+    width: 100%;
+    height: 4rem;
+    margin-top: 0;
+    background-color: #353535;
+    color: white;
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    > h1 {
+      position: absolute;
+      left: 0.5rem;
+    }
+    > h2 {
+      position: absolute;
+      right: 0.5rem;
+    }
+  }
+  > .game {
+    display: grid;
+    grid-template-columns: repeat(16, 1fr);
+    grid-template-rows: repeat(16, 1fr);
+    aspact-ratio: 1;
+    margin: 0 auto;
+    height: calc(100vh - 5rem);
+    grid-gap: 0.5rem;
+    padding: 0.5rem;
+    margin-top: 4rem;
+    color: white;
+    > * {
+      background-color: #754565;
+      border-radius: .5rem;
+  }
+`;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: false,
+      error: false,
+      username: "Ewsgit"
+    };
+  }
   render() {
     return (
-      <div className="h-[100vh]">
-        <div className="w-auto h-[calc(15*0.25rem)] flex justify-start items-center bg-gray-900">
-          <h1 className="text-white text-center ml-5 text-xl">Hello World</h1>
+      <Page>
+        <div className="nav">
+          <h1>Energix</h1>
+          <h2>{this.state.username}</h2>
         </div>
-        <div className="h-[calc(100vh-3.75rem)] w-auto grid grid-cols-8 grid-rows-[repeat(8,minmax(0,1fr))] bg-gray-700">
-          <h1>Hello</h1>
+        <div className="game">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-      </div>
+      </Page>
     );
   }
 }
